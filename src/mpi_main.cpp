61d9fa53ab3eef46560be1f8c94f110b75355d81
Semih Gür <semihgur201@gmail.com>
2023-04-18 18:04:57 +0300

    LINE 184
    image rescale eden func yarıya kadar hesapladıktan sonra memory out of bound yiyor

--- src/mpi_main.cpp
+++ src/mpi_main.cpp
@@ -8,17 +8,16 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
-
 #include "stb_image.h"
 #include "stb_image_write.h"
 
-long get_current_core_frequency();
-void downscaling(uint8_t* rgb_image,int width, int height,uint8_t* downsampled_image);
+void downscaling(uint8_t *rgb_image, int width, u_int64_t height, uint8_t *downsampled_image);
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
-    // Inıt MPI 
-    MPI_Init(&argc,&argv);
+    // Inıt MPI Bismillahirrahmaniraahhimm
+    MPI_Init(&argc, &argv);
 
     // Determine rank and size (process count)
     int rank, size;
@@ -26,131 +25,105 @@ int main(int argc, char* argv[]){
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 
-    // Image Informations
-    int width, height, channels;
-    unsigned char *input_image ;
-    int output_width;
-    int output_height;
+    if (rank == 0)
+    { // Master process
+        // Image Informations
+        int width, height, channels;
+        unsigned char *input_image;
+        int output_width;
+        int output_height;
 
-    unsigned char *input_chunk;
-
-
-
-    if(rank == 0){  // Master process
         // Read input image
-
-        input_image = stbi_load(argv[1],&width,&height,&channels,0);
-
+        
+        input_image = stbi_load(argv[1], &width, &height, &channels, 0);    
+        channels=4;
         output_width = width / 2;
         output_height = height / 2;
 
-        printf("Width: %d  Height: %d \n",width,height);
-      
+        printf("Width: %d  Height: %d \n", width, height);
 
-        // Receive frequency information from worker processes
-        long *frequencies = (long*)malloc((size - 1) * sizeof(long));
-        for (int i =1 ; i< size ; i++){
-            // Recive all of them from other proccessor
-            MPI_Recv(&frequencies[i - 1 ], 1, MPI_LONG,i , 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-            //printf("%ld\n", frequencies[i - 1 ]);
-
+        // Calculate workload for each rank
+        int* divided_line_count = (int *)malloc((size) * sizeof(int));
+        for(int i=0;i<size;i++){
+            divided_line_count[i] = height / size;
+        }
+        divided_line_count[size-1] += height % size;
+        //prining divided line count
+        for(int i=0;i<size;i++){
+            printf("Divided line count: %d \n", divided_line_count[i]);
         }
 
-        
-        // Master process de imagge proseesing yapcak
-        
 
-        // Calculate total frequency
-        long total_frequency = 0;
-        for (int i = 0; i < size - 1; i++) {
-            total_frequency += frequencies[i];
-        }
-        
 
-        // Calculate workload distribution
-        int *workload_sizes = (int*)malloc((size - 1) * sizeof(int));
-        for (int i = 0; i < size - 1; i++) {
-            workload_sizes[i] = (height / size) * (float)(frequencies[i] * ( 1.0 / total_frequency));
-        }
-
-        
         // Send image portions to worker processes
-        int offset = 0;
-        for (int i = 1; i < size; i++) {
-            int chunk_size = workload_sizes[i - 1] * width * channels;
-            MPI_Send(&chunk_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&input_image[offset], chunk_size, MPI_UNSIGNED_CHAR, i, 0, MPI_COMM_WORLD);
-            offset += chunk_size;
-        }
-
-        // Send width of immage to other processor
-        for (int i = 1; i < size; i++) {
+        int offset = divided_line_count[0] * width * channels;
+        for (int i = 1; i < size; i++)
+        {
+            int eight_bit_int_count = divided_line_count[i] * width * channels;
+            MPI_Send(&eight_bit_int_count, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            printf("input_image[offset]: %d \n", offset);
+            MPI_Send(&input_image[offset], eight_bit_int_count, MPI_UINT8_T, i, 0, MPI_COMM_WORLD);
             MPI_Send(&width, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            offset += eight_bit_int_count;
         }
+        u_int8_t *output_image = (u_int8_t *)malloc(output_width * output_height * channels * sizeof(u_int8_t));
+        printf("Chunk sizaaaaaaaaaaaaaaaaaaaaaaaaaaaaae: %d \n", divided_line_count[0] * width * channels);
 
-        /*
+        downscaling(input_image, width,(u_int64_t) divided_line_count[0] * width * channels, output_image);
+
+
+
+        
         // Receive downscaled image portions and combine them into a single image
-        unsigned char *output_image = (unsigned char *)malloc(output_width * output_height * channels);
-        int output_offset = 0;
+        int output_offset = output_width * divided_line_count[0] * channels;
+        int output_eight_bit_int_count;
         for (int i = 1; i < size; i++) {
-            int output_chunk_size = output_width * workload_sizes[i - 1] * channels;
-            MPI_Recv(&output_image[output_offset], output_chunk_size, MPI_UNSIGNED_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            output_offset += output_chunk_size;
+            output_eight_bit_int_count = divided_line_count[i] * output_width * channels;
+            MPI_Recv(&output_image[output_offset], output_eight_bit_int_count, MPI_UINT8_T, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            output_offset += output_eight_bit_int_count;
         }
 
         // Save the output image
-        stbi_write_jpg(argv[3], output_width, output_height, channels, output_image, output_width * channels);
+        stbi_write_jpg(argv[2], output_width, output_height, channels, output_image, output_width * channels);
         free(output_image);
 
-        */
+        
         // Clean up
         free(input_image);
-        free(frequencies);
-        //free(workload_sizes);
-        
 
+        // free(workload_Ratios);
+    }
+    else
+    {
 
-    }else{
-
-
-        // Get performance info and sent it to Master process
-        long frequencyOfCurrent = get_current_core_frequency();
-
-
-        // Send frequency to Master process to recive partition to handle.
-        MPI_Send(&frequencyOfCurrent, 1 , MPI_LONG, 0,0,MPI_COMM_WORLD);
-        
-        
-        
         // Receive the assigned image portion and its size
-        int assigned_chunk_size;
-        MPI_Recv(&assigned_chunk_size, 1 , MPI_INT, 0 , 0 ,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        
-        unsigned char *input_chunk = (unsigned char *)malloc(assigned_chunk_size );
-        MPI_Recv(input_chunk, assigned_chunk_size, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-
+        int assigned_eight_bit_int_count;
+        MPI_Recv(&assigned_eight_bit_int_count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Assigned eight bit int count: %d \n", assigned_eight_bit_int_count);
+        uint8_t*input_chunk = (uint8_t*)malloc(assigned_eight_bit_int_count*sizeof(uint8_t));
+        printf("Input ch11111111111111k: %d \n", input_chunk[0]);
+        MPI_Recv(input_chunk, assigned_eight_bit_int_count, MPI_UINT8_T, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Input chu22222222222nk: %d \n", input_chunk[0]);
         // recieve width from master proccess
         int width;
-        MPI_Recv(&width, 1 , MPI_INT, 0 , 0 ,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&width, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Width:aaaaaaaaaaaa %d \n", width);  
 
         // Donwscale image partition
         // Downscale the assigned image portion
-        int output_chunk_size = assigned_chunk_size / 4;
-        unsigned char *output_chunk = (unsigned char *)malloc(output_chunk_size);
-        
+        unsigned char *output_chunk = (unsigned char *)malloc(assigned_eight_bit_int_count / 4);
+
         /* Segment foult here */
-        downscaling(input_chunk, width ,assigned_chunk_size,output_chunk);
-        
+        printf("Chunk size: %d \n", assigned_eight_bit_int_count);
+        downscaling(input_chunk, width, assigned_eight_bit_int_count, output_chunk);
+
         // Send the downscaled image portion back to the master process
-        //MPI_Send(output_chunk, output_chunk_size, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(output_chunk, assigned_eight_bit_int_count, MPI_UINT8_T, 0, 0, MPI_COMM_WORLD);
 
         // Clean up
         free(input_chunk);
         free(output_chunk);
-        
     }
-
 
     /*
     // Save and Clean up for the master process
@@ -160,76 +133,56 @@ int main(int argc, char* argv[]){
 
         free(input_image);
     }*/
-    
 
     // Finalize MPI
     MPI_Finalize();
 
     return 0;
-
 }
-
-
-/*
-    This function return current processor frequency.
-    So we can determine the performance of the proccesor.
-    Because its frequency is directly proportional to its performance.s
-*/
-long get_current_core_frequency() {
-    FILE *cpuinfo = fopen("/proc/cpuinfo", "rb");
-    if (cpuinfo == NULL) {
-        perror("Failed to open /proc/cpuinfo");
-        return -1;
-    }
-
-    long frequency = -1;
-    char line[256];
-    while (fgets(line, sizeof(line), cpuinfo)) {
-        if (sscanf(line, "cpu MHz : %ld", &frequency) == 1) {
-            break;
-        }
-    }
-
-    fclose(cpuinfo);
-
-    return frequency;
-}
-
-
-
 
 // Got segmentation fould should be fix
-void downscaling(uint8_t* rgb_image,int width, int chunkSize,uint8_t* downsampled_image)
-{	
+void downscaling(uint8_t *rgb_image, int width, u_int64_t chunkSize, uint8_t *downsampled_image)
+{
     int downsampled_width = width / 2;
-    int downsampled_height = chunkSize / (width  / 4 /* Channel */) ;
+    printf("witdth: %d\nChunksize: %ld\n", width, chunkSize);
+    //divided_line_count[0] * width * channels
+    int downsampled_height = (chunkSize / width)/8;
+    printf("downsampled_width: %d\n", downsampled_width);
+    printf("downsampled_height: %d\n", downsampled_height);
+    int a=0;
 
     // iterate through the image pixels in blocks of size 2x2 and calculate the average
-    for (int y = 0; y < downsampled_height; y++) {
-        for (int x = 0; x < downsampled_width; x++) {
+    for (int y = 0; y < downsampled_height; y++)
+    {
+        for (int x = 0; x < downsampled_width; x++)
+        {
             int r = 0, g = 0, b = 0, a = 0;
 
             // calculate the average value of each channel in the 2x2 block
-            for (int dy = 0; dy < 2; dy++) {
-                for (int dx = 0; dx < 2; dx++) {
+            for (int dy = 0; dy < 2; dy++)
+            {
+                for (int dx = 0; dx < 2; dx++)
+                {
                     int pixel_index = ((y * 2 + dy) * width + (x * 2 + dx)) * 4;
-                    /* SEGFAULT HERE
-                    // r += rgb_image[pixel_index];
-                    // g += rgb_image[pixel_index + 1];
-                    // b += rgb_image[pixel_index + 2];
-                    // a += rgb_image[pixel_index + 3];
-                    */
+                    r += rgb_image[pixel_index];
+                    g += rgb_image[pixel_index + 1];
+                    b += rgb_image[pixel_index + 2];
+                    a += rgb_image[pixel_index + 3];
+                    
                 }
             }
 
             // calculate the average value for each channel and store it in the downsampled image
             int downsampled_pixel_index = (y * downsampled_width + x) * 4;
-            /* SEGFAULT HERE
-            // downsampled_image[downsampled_pixel_index] = r / 4;
-            // downsampled_image[downsampled_pixel_index + 1] = g / 4;
-            // downsampled_image[downsampled_pixel_index + 2] = b / 4;
-            // downsampled_image[downsampled_pixel_index + 3] = a / 4;
-            */
+
+            downsampled_image[downsampled_pixel_index] = r / 4;
+            downsampled_image[downsampled_pixel_index + 1] = g / 4;
+            downsampled_image[downsampled_pixel_index + 2] = b / 4;
+            downsampled_image[downsampled_pixel_index + 3] = a / 4;
         }
+        a++;
+        printf("Downscaling done%d\n",a);
+
     }
+    printf("Downscaling done\n");
 }
