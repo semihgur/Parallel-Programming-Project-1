e1358974e3d5b29a34a93bcfa76be13a02ac5bdc
Emirhan Akıtürk <emirhanakiturk@gmail.com>
2023-04-28 22:44:21 +0300

    fnl

--- src/mpi_main.cpp
+++ src/mpi_main.cpp
@@ -1,169 +1,128 @@
+/**
+ *
+ * CENG342 Project-1
+ *
+ * Downscaling 
+ *
+ * Usage:  main <input.jpg> <output.jpg> 
+ *
+ * @group_id 00
+ * @author  your names
+ *
+ * @version 1.0, 02 April 2022
+ */
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#include <sched.h>
-#include <hwloc.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include "stb_image.h"
 #include "stb_image_write.h"
+#define CHANNEL_NUM 4
 
-void downscaling(uint8_t *rgb_image, int width, int chunkize, uint8_t *downsampled_image);
-
-int main(int argc, char *argv[])
-{
-
-    // InÄ±t MPI Bismillahirrahmaniraahhimm
-    MPI_Init(&argc, &argv);
-
-    // Determine rank and size (process count)
+void paralel_downscaling(uint8_t* rgb_image, int width, int height, uint8_t* downsampled_image, int rank, int size);
+//Do not use global variables
+int main(int argc, char* argv[]) 
+{   
     int rank, size;
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    int width, height, bpp;
 
-    if (rank == 0)
-    { // Master process
-        // Image Informations
-        int width, height, channels=4;
-        unsigned char *input_image;
-        int output_width;
-        int output_height;
+    // Reading the image in RGBA colors
+    uint8_t* input_image = stbi_load(argv[1], &width, &height, &bpp, CHANNEL_NUM);
 
-        // Read input image
-        
-        input_image = stbi_load(argv[1], &width, &height, &channels, 4);    
-        channels=4;
-        output_width = width / 2;
-        output_height = height / 2;
-
+    if (rank == 0) {
         printf("Width: %d  Height: %d \n", width, height);
-
-        // Calculate workload for each rank
-        int* divided_line_count = (int *)malloc((size) * sizeof(int));
-        for(int i=0;i<size;i++){
-            divided_line_count[i] = height / size;
-        }
-        divided_line_count[size-1] += height % size;
-        //prining divided line count
-        for(int i=0;i<size;i++){
-            printf("Divided line count: %d \n", divided_line_count[i]);
-        }
-
-
-
-        // Send image portions to worker processes
-        int offset = divided_line_count[0] * width * channels;
-        for (int i = 1; i < size; i++)
-        {
-            int eight_bit_int_count = divided_line_count[i] * width * channels;
-            MPI_Send(&eight_bit_int_count, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&input_image[offset], eight_bit_int_count, MPI_UINT8_T, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&width, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            offset += eight_bit_int_count;
-        }
-        u_int8_t *output_image = (u_int8_t *)malloc(output_width * output_height * channels * sizeof(u_int8_t));
-        downscaling(input_image, width,divided_line_count[0] * width * channels, output_image);
-        printf("denememejeeek");
-
-
-        
-        // Receive downscaled image portions and combine them into a single image
-        int output_offset = output_width * divided_line_count[0] * channels;
-        int output_eight_bit_int_count;
-        for (int i = 1; i < size; i++) {
-            output_eight_bit_int_count = divided_line_count[i] * output_width * channels;
-            MPI_Recv(&output_image[output_offset], output_eight_bit_int_count, MPI_UINT8_T, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            output_offset += output_eight_bit_int_count;
-        }
-
-        // Save the output image
-        stbi_write_jpg(argv[2], output_width, output_height, channels, output_image, output_width * channels);
-        free(output_image);
-
-        
-        // Clean up
-        free(input_image);
-
-        // free(workload_Ratios);
-    }
-    else
-    {
-
-        // Receive the assigned image portion and its size
-        int assigned_eight_bit_int_count;
-        MPI_Recv(&assigned_eight_bit_int_count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        uint8_t*input_chunk = (uint8_t*)malloc(assigned_eight_bit_int_count*sizeof(uint8_t));
-
-        MPI_Recv(input_chunk, assigned_eight_bit_int_count, MPI_UINT8_T, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        // recieve width from master proccess
-        int width;
-        MPI_Recv(&width, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  
-
-        // Donwscale image partition
-        // Downscale the assigned image portion
-        uint8_t *output_chunk = (uint8_t *)malloc(assigned_eight_bit_int_count * sizeof(uint8_t));
-
-        /* Segment foult here */
-        printf("Rank: %d width: %d assigned_eight_bit_int_count: %d\n",rank,width,assigned_eight_bit_int_count);
-        downscaling(input_chunk, width, assigned_eight_bit_int_count, output_chunk);
-        printf("Chunk siasdasdasdasdÅŸlkashjdlkjhasgbdlkjhasgdkjlaze: %d \n", assigned_eight_bit_int_count);
-        // Send the downscaled image portion back to the master process
-        MPI_Send(output_chunk, assigned_eight_bit_int_count, MPI_UINT8_T, 0, 0, MPI_COMM_WORLD);
-
-        // Clean up
-        free(input_chunk);
-        free(output_chunk);
+        printf("Input: %s , Output: %s  \n", argv[1], argv[2]);
     }
 
-    // Finalize MPI
+    // start the timer
+    double time1 = MPI_Wtime();   
+
+    uint8_t* downsampled_image = (uint8_t*)malloc(width/2 * height/2 * CHANNEL_NUM * sizeof(uint8_t));
+    paralel_downscaling(input_image, width, height, downsampled_image, rank, size);
+
+    double time2 = MPI_Wtime();
+    if (rank == 0) {
+        printf("Elapsed time: %lf \n", time2 - time1);    
+        // Storing the image 
+        stbi_write_jpg(argv[2], width/2, height/2, CHANNEL_NUM, downsampled_image, 100);
+    }
+
+    stbi_image_free(input_image);
+    stbi_image_free(downsampled_image);
+
     MPI_Finalize();
-
     return 0;
 }
 
-// Got segmentation fould should be fix
-void downscaling(uint8_t *rgb_image, int width,int chunkSize, uint8_t *downsampled_image)
-{
+
+void paralel_downscaling(uint8_t* rgb_image, int width, int height, uint8_t* downsampled_image, int rank, int size) {
     int downsampled_width = width / 2;
-    printf("witdth: %d\nChunksize: %d\n", width, chunkSize);
-    //divided_line_count[0] * width * channels
-    int downsampled_height = (chunkSize / width)/8;
-    printf("downsampled_width: %d\n", downsampled_width);
-    printf("downsampled_height: %d\n", downsampled_height);
+    int downsampled_height = height / 2;
 
-    // iterate through the image pixels in blocks of size 2x2 and calculate the average
-    for (int y = 0; y < downsampled_height; y++)
-    {
-        for (int x = 0; x < downsampled_width; x++)
-        {
-            int r = 0, g = 0, b = 0, a = 0;
+    int pixels_per_process = (downsampled_width * downsampled_height) / size;
+    int start_index = rank * pixels_per_process;
+    int end_index = start_index + pixels_per_process;
 
-            // calculate the average value of each channel in the 2x2 block
-            for (int dy = 0; dy < 2; dy++)
-            {
-                for (int dx = 0; dx < 2; dx++)
-                {
-                    int pixel_index = ((y * 2 + dy) * width + (x * 2 + dx)) * 4;
-                    r += rgb_image[pixel_index];
-                    g += rgb_image[pixel_index + 1];
-                    b += rgb_image[pixel_index + 2];
-                    a += rgb_image[pixel_index + 3];
-                    
-                }
-            }
-            // calculate the average value for each channel and store it in the downsampled image
-            int downsampled_pixel_index = (y * downsampled_width + x) * 4;
-
-            downsampled_image[downsampled_pixel_index] = r / 4;
-            downsampled_image[downsampled_pixel_index + 1] = g / 4;
-            downsampled_image[downsampled_pixel_index + 2] = b / 4;
-            downsampled_image[downsampled_pixel_index + 3] = a / 4;
-        }
+    if (rank == size - 1) {
+        end_index = downsampled_width * downsampled_height;
     }
+
+    // iterate through the assigned image pixels in blocks of size 2x2 and calculate the average
+    for (int i = start_index; i < end_index; i++) {
+        int x = i % downsampled_width;
+        int y = i / downsampled_width;
+
+        int r = 0, g = 0, b = 0, a = 0;
+
+        // calculate the average value of each channel in the 2x2 block
+        for (int dy = 0; dy < 2; dy++) {
+            for (int dx = 0; dx < 2; dx++) {
+                int pixel_index = ((y * 2 + dy) * width + (x * 2 + dx)) * 4;
+
+                r += rgb_image[pixel_index];
+                g += rgb_image[pixel_index + 1];
+                b += rgb_image[pixel_index + 2];
+                a += rgb_image[pixel_index + 3];
+            }
+        }
+
+        // calculate the average value for each channel and store it in the downsampled image
+        int downsampled_pixel_index = (y * downsampled_width + x) * 4;
+            
+        downsampled_image[downsampled_pixel_index] = r / 4;
+        downsampled_image[downsampled_pixel_index + 1] = g / 4;
+        downsampled_image[downsampled_pixel_index + 2] = b / 4;
+        downsampled_image[downsampled_pixel_index + 3] = a / 4;
+    }
+
+    // gather the downsampled image from all processes and combine into the final output image
+    MPI_Allgather(MPI_IN_PLACE, pixels_per_process * CHANNEL_NUM, MPI_UNSIGNED_CHAR,
+                  downsampled_image, pixels_per_process * CHANNEL_NUM, MPI_UNSIGNED_CHAR,
+                  MPI_COMM_WORLD);
 }
+
+/*
+Resmi eþit parçalara bölüyor process sayýsýna göre
+int piece_size = (width * height) / size;
+
+int start_index = rank * piece_size;
+int end_index = start_index + piece_size;
+
+uint8_t* my_image_piece = (uint8_t*)malloc(piece_size * CHANNEL_NUM * sizeof(uint8_t));
+
+for (int i = start_index; i < end_index; i++) {
+    my_image_piece[i - start_index] = input_image[i];
+}
+*/
+
+
+
